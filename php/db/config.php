677245b4194cb2f07a92a88242e207694c37c2db
willie_wang <willie_wang@sinobasedm.com>
2017-09-26 12:02:09 +0800

    添加mysql 操作数据相关文件

---
+++ php/db/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/26
+ * Time: 11:39
+ */
+namespace DbConfig {
+    interface Config {
+        // MY SQL配置
+        const MYSQL_HOST = '124.239.180.30';
+        const MYSQL_DB_NAME = 'study';
+        const MYSQL_USER_NAME = 'dev_rw';
+        const MYSQL_USER_PASSWORD = '4VCK8lVM9qC';
+    }
+}

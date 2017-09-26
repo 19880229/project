aca2f594ea7ecb48798d30f96ad36e8eb33e5d7d
hly <1065925836@qq.com>
2017-09-26 13:39:05 +0800

    kk

--- php/hly.php
+++ php/hly.php
@@ -1,9 +1,15 @@
 <?php
 $x=5;
-$y=6;
+$y=10;
 $z=$x+$y;
+$c=$y-$x;
+$m=$x*$y;
+$n=$y/$x;
 echo $z."\r\n";
-$peoples = ["zhangsan", "lisi"];
+echo $c."\r\n";
+echo $m."\r\n";
+echo $n."\r\n";
+
 
 function calculate() {
     $peoples = ["zhangsan", "lisi"];
@@ -16,3 +22,43 @@ function calculate() {
     }
 }
 calculate();
+
+function add($x = 0, $y = 0) {
+
+    return $x + $y;
+}
+$result = add(10, 5);
+
+//输出
+echo "相加结果：".$result."\r\n";
+function delete($x = 0, $y = 0) {
+
+    return $x - $y;
+}
+$result = delete(10, 5);
+
+//输出
+echo "相减结果：".$result."\r\n";
+
+function mul($x = 0, $y = 0) {
+
+    return $x * $y;
+}
+$result = mul(10, 5);
+
+//输出
+echo "相乘结果：".$result."\r\n";
+
+
+$z=5;
+$t=15;
+if($t+$z<10){
+    echo "Have a good day!";
+
+}
+elseif($t+$z>30){
+    echo "Have a good night!";
+}else
+{
+    echo "Have a good morning!";
+}

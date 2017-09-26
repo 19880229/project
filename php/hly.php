ab6137a45c9f12fa4e339f32a4e292032cbdc523
hly <1065925836@qq.com>
2017-09-26 11:31:08 +0800

    ll

--- php/hly.php
+++ php/hly.php
@@ -2,4 +2,17 @@
 $x=5;
 $y=6;
 $z=$x+$y;
-echo $z;
+echo $z."\r\n";
+$peoples = ["zhangsan", "lisi"];
+
+function calculate() {
+    $peoples = ["zhangsan", "lisi"];
+    if (empty($peoples)) {
+        echo "数据为空";
+    } else {
+        foreach ($peoples as $item) {
+            echo $item."\r\n";
+        }
+    }
+}
+calculate();

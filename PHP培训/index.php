b2ebd79101108595b45098be1ae50b9083227b55
willie_wang <willie_wang@sinobasedm.com>
2017-09-26 10:28:16 +0800

    修改

--- PHP培训/index.php
+++ PHP培训/index.php
@@ -1,1 +1,44 @@
 <?php
+
+
+//变量定义
+$a;//语法对尽量不要这样写
+$a = 0;//正常用法
+
+//数据类型 int float array  string  object
+$name = "willie";
+$count = 0;
+$peoples = ["zhangsan", "lisi"];
+$people = new stdClass();
+$people->name = "name";
+
+
+//自定义函数(方法)
+//有返回值
+function add($x = 0, $y = 0) {
+
+    return $x + $y;
+}
+
+//无返回值
+function voidAdd(&$x, &$y) {
+    $x++;
+    $y++;
+}
+
+//方法调用
+$result = add(10, 5);
+//输出
+echo $result;
+
+//PHP if else
+function calculate() {
+    if (empty($peoples)) {
+        echo "数据为空";
+    } else {
+        foreach ($peoples as $item) {
+           echo $item;
+        }
+    }
+}
+

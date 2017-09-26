613a7e41582e8b56758d0338e5e07cf1012762e4
xian509 <1041623374@qq.com>
2017-09-26 11:30:01 +0800

    1

--- php/yxx.php
+++ php/yxx.php
@@ -5,4 +5,17 @@
  * Date: 2017/9/26
  * Time: 10:43
  */
-function 
+
+$num1=0;
+$num2=0;
+$num3=0;
+
+
+//加法
+function Add($num1,$num2){
+    global $num1,$num2;
+    $num3=$num1+$num2;
+
+}
+$result = Add(10, 5);
+echo"相加结果：".$result."\r\n";
